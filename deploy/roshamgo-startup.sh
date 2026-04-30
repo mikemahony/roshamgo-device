cc7fb697f789b0094d47e9e8f372ab6144e957fb
Mike Mahony <mikemahony@pixar.com>
2026-04-30 10:19:41 -0700

    Xorg conf to disable screensaver, flash=false, service file update

--- deploy/roshamgo-startup.sh
+++ deploy/roshamgo-startup.sh
@@ -100,12 +100,30 @@ check_existing_binary() {
 
 start_x() {
     if [ -z "${DISPLAY:-}" ]; then
+        # Write xorg config to disable screensaver/DPMS at server level
+        mkdir -p /etc/X11/xorg.conf.d
+        cat > /etc/X11/xorg.conf.d/10-no-screensaver.conf << 'XCONF'
+Section "ServerFlags"
+    Option "BlankTime" "0"
+    Option "StandbyTime" "0"
+    Option "SuspendTime" "0"
+    Option "OffTime" "0"
+EndSection
+
+Section "ServerLayout"
+    Option "BlankTime" "0"
+    Option "StandbyTime" "0"
+    Option "SuspendTime" "0"
+    Option "OffTime" "0"
+EndSection
+XCONF
+
         log "Starting X server..."
         Xorg :0 -nolisten tcp &
         X_PID=$!
         export DISPLAY=:0
         sleep 3
-        # Disable screen saver, blanking, and DPMS
+        # Belt and suspenders: also disable via xset
         DISPLAY=:0 xset s off 2>/dev/null || true
         DISPLAY=:0 xset s noblank 2>/dev/null || true
         DISPLAY=:0 xset -dpms 2>/dev/null || true
